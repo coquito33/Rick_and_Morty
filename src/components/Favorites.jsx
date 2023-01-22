ca1c8e2b12619b807ff4e8a45a3bd2849754cb10
coquito33 <amadomurillo12@gmail.com>
2023-01-22 06:15:56 -0500

    homework 13 React-Hooks

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,8 +1,33 @@
 import { connect, Connect } from "react-redux";
 import Card from "./Card";
+import { useDispatch } from "react-redux";
+import { orderCards } from "../redux/actions";
+import { filterCards } from "../redux/actions";
 
 const Favorites = ({myFavorites}) => {
+
+    const dispatchFavorites = useDispatch();
+
+    const handleChangeFavorites = (event) => {
+
+        if(event.target.name==="order") dispatchFavorites(orderCards(event.target.value));
+        if(event.target.name==="gender") dispatchFavorites(filterCards(event.target.value));
+    }
+
     return(
+        <div>
+        <div>
+            <select name="order" onChange={handleChangeFavorites} >
+                <option value="Ascendiente">Ascendiente</option>
+                <option value="Descendiente">Descendiente</option>
+            </select>
+            <select name="gender" onChange={handleChangeFavorites} >
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Genderless">Genderless</option>
+                <option value="unknown">unknown</option>
+            </select>
+        </div>
         <div>
             {myFavorites.map((card)=>{
                 return(
@@ -18,6 +43,7 @@ const Favorites = ({myFavorites}) => {
                     
                  )
             })}
+        </div>
         </div>
     )
 }
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,12 +1,13 @@
 import { useReducer } from "react";
-import { ADD_FAVORITE } from "./actions";
+import { ADD_FAVORITE, ORDER_CARDS } from "./actions";
 import { DELETE_FAVORITE } from "./actions";
-
+import { FILTER_CARDS } from "./actions";
 
 
 
 const initialState = {
     myFavorites: [],
+    allCharacters: []
 };
 
  const reducer = (state = initialState, action) => {
@@ -15,16 +16,35 @@ const initialState = {
         case ADD_FAVORITE: console.log("llego al reducer add")
             return {
                 ...state,
-                myFavorites: [...state.myFavorites, action.payload]
+                myFavorites: [...state.myFavorites, action.payload],
+                allCharacters: [...state.allCharacters, action.payload]
             }
             
         case DELETE_FAVORITE:
-            let filterMyFavorites = state.myFavorites.filter((card) => card.id !== action.payload)
+            let filterMyFavorites = state.myFavorites.filter((card) => card.id !== action.payload);
+            let filterAllCharacters = state.allCharacters.filter((card) => card.id !== action.payload);
             return {
                 ...state,
-                myFavorites: filterMyFavorites
+                myFavorites: filterMyFavorites,
+                allCharacters: filterAllCharacters
             }
         
+        case FILTER_CARDS:
+            let filterAllCharactersForGender = state.allCharacters.filter((cards) => cards.gender === action.payload );
+            return{
+                ...state,
+                myFavorites: filterAllCharactersForGender
+            }
+
+        case ORDER_CARDS:
+            let copiaAllCharacters = [...state.allCharacters];
+            if(action.payload==="Ascendiente") copiaAllCharacters.sort((a, b)=> a.id-b.id)
+            if(action.payload==="Descendiente") copiaAllCharacters.sort((a, b)=> b.id-a.id)
+            return{
+                ...state,
+                myFavorites: copiaAllCharacters
+            }
+
         default:{
             return { ...state };
         }
